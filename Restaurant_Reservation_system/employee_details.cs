26ca2809e4043f8a7b20096bbf5ad565668b2481
kalana ranathunga <kalanaranathunga5@gmail.com>
2024-02-16 15:41:58 +0530

    data grid view

---
+++ Restaurant_Reservation_system/employee_details.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant_Reservation_system
+{
+    public partial class employee_details : Form
+    {
+        public employee_details()
+        {
+            InitializeComponent();
+        }
+
+        private void employee_details_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
